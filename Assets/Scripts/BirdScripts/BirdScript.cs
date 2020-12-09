1aa9cddadc75a4105dc58f963c741004303dea32
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-09 22:37:26 +0530

    bird's movement and flapping completed in the bird script

---
+++ Assets/Scripts/BirdScripts/BirdScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public static BirdScript instance;
+
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+    [SerializeField]
+    private Animator anim;
+    private float forwardSpeed = 3f;
+    private float bounceSpeed = 4f;
+
+    private bool didFlap;
+    public bool isAlive;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        isAlive = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAlive)
+        {
+            Vector3 temp = transform.position;
+            temp.x += forwardSpeed * Time.deltaTime;
+            transform.position = temp;
+        }
+
+        if (didFlap)
+        {
+            didFlap = false;
+            myRigidBody.velocity = new Vector2(0, bounceSpeed);
+            anim.SetTrigger("Flap");
+        }
+    }
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
+    }
+}
