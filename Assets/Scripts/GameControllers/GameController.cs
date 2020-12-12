f9ab2df57e640c64814f67914d978f21b995469b
sdoshi983 <sdoshi983@rku.ac.in>
2020-12-12 11:09:47 +0530

    Menu controller added and birds changing enabled

--- Assets/Scripts/GameControllers/GameController.cs
+++ Assets/Scripts/GameControllers/GameController.cs
@@ -40,8 +40,8 @@ public class GameController : MonoBehaviour
         {
             PlayerPrefs.SetInt(HIGH_SCORE, 0);
             PlayerPrefs.SetInt(SELECTED_BIRD, 0);
-            PlayerPrefs.SetInt(RED_BIRD, 0);
-            PlayerPrefs.SetInt(GREEN_BIRD, 0);
+            PlayerPrefs.SetInt(RED_BIRD, 1);
+            PlayerPrefs.SetInt(GREEN_BIRD, 1);
             PlayerPrefs.SetInt("IsTheGameStartedForTheFirstTime", 0);
         }
     }
